d3d9c3858910003e7653eee29b016b68ccbfb862
Dan Sackett <danesackett@gmail.com>
2013-07-18 19:25:59 -0400

    Add reverse text function

---
+++ text/reverse.py
@@ -0,0 +1,14 @@
+"""
+Reverse a String
+- Enter a string and the program will reverse it and print it out.
+
+Call By
+- python
+- from text.reverse import reverse
+- reverse('STRING')
+
+"""
+
+
+def reverse(string):
+    print string[::-1]
